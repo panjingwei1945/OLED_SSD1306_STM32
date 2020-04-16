0830dbe691136bb3cf512de703264f1f115444e9
panjingwei1945 <panjingwei1945@hotmail.com>
2020-04-16 14:57:24 +0800

    Add library files.

---
+++ oledfont.h
@@ -0,0 +1,10 @@
+#ifndef __OLEDFONT_H
+#define __OLEDFONT_H 	   
+
+extern const unsigned char F8X16[];
+
+extern const unsigned char F6x8[][6];
+
+#endif
+
+
---
+++ ssd1306.h
@@ -0,0 +1,59 @@
+//////////////////////////////////////////////////////////////////////////////////
+//本程序只供学习使用，未经作者许可，不得用于其它任何用途
+//使用SysTick的普通计数模式对延迟进行管理
+//包括delay_us,delay_ms
+//中景园电子
+//店铺地址：http://shop73023976.taobao.com/shop/view_shop.htm?tracelog=twddp&trace_log=qianniu_pc
+//修改日期:2014/3/16
+//版本：V1.0
+//版权所有，盗版必究。
+//Copyright(C) 中景园电子2014/3/16
+//All rights reserved
+//********************************************************************************
+#ifndef __OLED_H
+#define __OLED_H
+//OLED模式设置
+//0:4线串行模式
+//1:并行8080模式
+#define OLED_MODE 0
+#define SIZE 16
+#define XLevelL		0x00
+#define XLevelH		0x10
+#define Max_Column	128
+#define Max_Row		64
+#define	Brightness	0xFF
+#define X_WIDTH 	128
+#define Y_WIDTH 	64
+//-----------------OLED端口定义----------------
+#define OLED_CS_Clr()  HAL_GPIO_WritePin(OLED_CS_GPIO_Port,OLED_CS_Pin,GPIO_PIN_RESET)
+#define OLED_CS_Set()  HAL_GPIO_WritePin(OLED_CS_GPIO_Port,OLED_CS_Pin,GPIO_PIN_SET)
+
+#define OLED_RST_Clr() HAL_GPIO_WritePin(OLED_RES_GPIO_Port,OLED_RES_Pin,GPIO_PIN_RESET)//RES
+#define OLED_RST_Set() HAL_GPIO_WritePin(OLED_RES_GPIO_Port,OLED_RES_Pin,GPIO_PIN_SET)
+
+#define OLED_DC_Clr() HAL_GPIO_WritePin(OLED_DC_GPIO_Port,OLED_DC_Pin,GPIO_PIN_RESET)
+#define OLED_DC_Set() HAL_GPIO_WritePin(OLED_DC_GPIO_Port,OLED_DC_Pin,GPIO_PIN_SET)
+
+#define OLED_CMD  0	//写命令
+#define OLED_DATA 1	//写数据
+
+
+//OLED控制用函数
+void OLED_WR_Byte(uint8_t dat,uint8_t cmd);
+void OLED_Display_On(void);
+void OLED_Display_Off(void);
+void OLED_Init(void);
+void OLED_Clear(void);
+void OLED_DrawPoint(uint8_t x,uint8_t y,uint8_t t);
+void OLED_Fill(uint8_t x1,uint8_t y1,uint8_t x2,uint8_t y2,uint8_t dot);
+void OLED_ShowChar(uint8_t x,uint8_t y,uint8_t chr,uint8_t size);
+void OLED_ShowNum(uint8_t x,uint8_t y,uint32_t num,uint8_t len,uint8_t size);
+void OLED_ShowString(uint8_t x,uint8_t y, uint8_t *p,uint8_t size);
+void OLED_Set_Pos(unsigned char x, unsigned char y);
+void OLED_ShowCHinese(uint8_t x,uint8_t y,uint8_t no,uint8_t (*Hz)[32]);
+void OLED_DrawBMP(unsigned char x0, unsigned char y0,unsigned char x1, unsigned char y1,unsigned char BMP[]);
+#endif
+
+
+
+
